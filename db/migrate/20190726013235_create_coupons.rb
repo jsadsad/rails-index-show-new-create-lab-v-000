f9af69abeb93ec6aa98ed9b8cb3d265065e51d71
Joshua Clarence Sadsad <joshsad2@gmail.com>
2019-07-26 01:33:27 +0000

    Automatically backed up by Learn

--- db/migrate/20190726013235_create_coupons.rb
+++ db/migrate/20190726013235_create_coupons.rb
@@ -4,7 +4,7 @@ class CreateCoupons < ActiveRecord::Migration[5.0]
       t.string :coupon_code
       t.string :store
       
-      t.timestamps null :false
+      t.timestamps null: false
     end
   end
 end
